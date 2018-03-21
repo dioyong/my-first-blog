180bec2335187f35c577678299c7be370cc090cf
dioyong <dioyong@gmail.com>
2018-03-21 23:51:11 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 # Create your views here.
@@ -8,5 +8,9 @@ def post_list(request):
     # 아래쿼리는 데이터가 추출이 안된다 조건이 안맞는듯 그래서 모든 데이터 불러오는 걸로 변경
     # 혹은 다른 filter 로 조건을 변경
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    # posts = Post.objects.filter(id=1)
+    #posts = Post.objects.filter(id=1)
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+# include 를 import 를 하지 않을 경우 에러 발생 장고 걸스 예제에는 이부분이 빠져 있다. 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
