e4a00f0f5d82c9c8f01aa1353b2f8784533a467a
dioyong <dioyong@gmail.com>
2018-03-27 09:41:50 +0900

    Add Comments Db and files

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-# include 를 import 를 하지 않을 경우 에러 발생 장고 걸스 예제에는 이부분이 빠져 있다. 
+from django.contrib.auth import views
+# include 를 import 를 하지 않을 경우 에러 발생 장고 걸스 예제에는 이부분이 빠져 있다.
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
